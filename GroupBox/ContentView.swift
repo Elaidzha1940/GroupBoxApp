a199f77c89f7c6e59104bc715a9b7172453e330c
Elaidzha1940 <eli080293@gmail.com>
2024-07-26 12:46:36 +0300

    GroupBoxApp

---
+++ GroupBox/ContentView.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: GroupBox
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 26.07.2024
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            GroupBox {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroupBox/GroupBoxApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: GroupBox
+//  File: GroupBoxApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 26.07.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct GroupBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
